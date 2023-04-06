3420b74b2ddf2a5a7410788e43df183658058b57
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-04-06 13:39:29 +0200

    pythonMegaCourse_lesson17 - Implementazione Edit Button

--- User_interface/gui.py
+++ User_interface/gui.py
@@ -6,12 +6,19 @@ label_instance = PySimpleGUI.Text("Metti qui i TO-DO")
 input_box_instance = PySimpleGUI.InputText(tooltip="suggerimento: Inserisci il ToDo",key="todo_key",do_not_clear=False)
                                                             # key è la chiave del dizionario
 add_button_instance = PySimpleGUI.Button("Add")
+list_box_instance = PySimpleGUI.Listbox(functions.get_todos(),key="list_todos_key",enable_events=True,size=[45,20])
+edit_button_instance = PySimpleGUI.Button("Edit")
 
 
 ########
 #WINDOW#
 window_instance = PySimpleGUI.Window("Titolo App"
-                            ,layout=[[label_instance],[input_box_instance,add_button_instance]]
+                            ,layout=
+                                [
+                                [label_instance]                            # riga1
+                                ,[input_box_instance,add_button_instance]   # riga2
+                                ,[list_box_instance,edit_button_instance]   # riga3
+                                ]
                             ,font=("Helvetica",20))
 
 # Se togliamo le [] interne otteniamo errore
@@ -37,6 +44,17 @@ while True:
             new_todo = event_tupla['todo_key'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window_instance["list_todos_key"].update(values=todos)
+        case "Edit":
+            todo_to_edit = event_tupla["list_todos_key"][0]
+            new_todo = event_tupla["todo_key"]
+            todos = functions.get_todos()
+            index= todos.index(todo_to_edit)
+            todos[index] = new_todo+"\n"
+            functions.write_todos(todos)
+            window_instance["list_todos_key"].update(values=todos)
+        case "list_todos_key":
+            window_instance["todo_key"].update(value=event_tupla["list_todos_key"][0])
         case PySimpleGUI.WIN_CLOSED:
             break
 
