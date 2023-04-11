495b099654ddd4f7372c3c6ffaf4928f94cf628a
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-04-11 19:45:57 +0200

    pythonMegaCourse_lesson19 - WebApp

--- web.py
+++ web.py
@@ -3,9 +3,13 @@ import modules.functions as functions
 #il comando per fare run di una web app è: --> streamlit run /home/lorenzo/pythonProjects/pythonMegaCourse_project1/web.py
 
 todos = functions.get_todos()
-for todo in todos:
-    print(f"Pippo: {todo}")
-    st.checkbox(todo)
+for todo_index,todo_value in enumerate(todos):
+    checkbox = st.checkbox(todo_value,key=todo_value)
+    if checkbox:
+        todos.pop(todo_index)
+        functions.write_todos(todos)
+        del st.session_state[todo_value]
+        st._rerun()
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -29,5 +33,6 @@ st.text_input(label=""
               , placeholder="Add new todo.."
               ,on_change=add_todo,key="new_todo")
 
-print("Hello")
+
+print ("Hello")
 st.session_state
