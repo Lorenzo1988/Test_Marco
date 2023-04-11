0f4f41d46745ad612528047c2435275ac0281052
lorenzo <lorenzo.chiappetta88@gmail.com>
2023-04-11 18:16:10 +0200

    pythonMegaCourse_lesson19 - Implement "Add New Todo item"

--- web.py
+++ web.py
@@ -2,6 +2,16 @@ import streamlit as st
 import modules.functions as functions
 #il comando per fare run di una web app è: --> streamlit run /home/lorenzo/pythonProjects/pythonMegaCourse_project1/web.py
 
+todos = functions.get_todos()
+for todo in todos:
+    print(f"Pippo: {todo}")
+    st.checkbox(todo)
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo APP")
 st.subheader("Lista di attività")
 st.text("ProvaProva")
@@ -13,9 +23,11 @@ verifica_todo= functions.verify_if_exist_file()
 print(verifica_todo)
 
 
-todos = functions.get_todos()
-for todo in todos:
-    print(f"Pippo: {todo}")
-    st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo..")
+
+st.text_input(label=""
+              , placeholder="Add new todo.."
+              ,on_change=add_todo,key="new_todo")
+
+print("Hello")
+st.session_state
